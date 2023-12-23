model works as intended. Next steps are: -fit power laws -fix the lack of short term memory due to embeddings -experiment with parameters to find best cost-variance tradeoff

source/memory.py
@@ -2,10 +2,8 @@ from collections import defaultdict
 import numpy as np
 import random
 
-
 from config import MEMORY_DIMENSION, N_NEIGHBOURS_IN_CONTEXT_EMBEDDING, SINGLE_WORD_EMBEDDING_DIMENSION, \
     NEIGHBOUR_SHARE_OF_INFO_IN_EMBEDDING, ALPHA
-
 
 
 # define the memory class
@@ -14,28 +12,37 @@ class Memory:
         self.number_of_cells = MEMORY_DIMENSION
         self.cell_dimension = SINGLE_WORD_EMBEDDING_DIMENSION
         self.memory = defaultdict(lambda: np.zeros(self.cell_dimension))
-        self.probabilities_of_removal = defaultdict(lambda: 0)
-    
+
     @property
     def occupied_cells(self):
         return len(self.memory)
-    
+
     @property
     def free_cells(self):
-        return self.number_of_cells-len(self.memory)
-    
-    def calculate_probabilities_of_removal(self):
-        pass
+        return self.number_of_cells - len(self.memory)
 
-    def randomly_choose_which_key_to_clear(self):
-        return random.choice(list(self.memory.keys()))
-    
+    def p_of_keeping_infos(self, current_position) -> np.array:
+        normalizing_factor = 0
+        p_of_keeping_infos = np.zeros(self.occupied_cells)
+        index = 0
+        for key, value in self.memory.items():
+            elapsed_words = current_position + 1 - key
+            p_of_keeping_infos[index] = (elapsed_words / (
+                        elapsed_words + 1)) ** ALPHA  # watch for current position values, should always start from one
+            normalizing_factor += p_of_keeping_infos[index]
+            index += 1
+        p_of_keeping_infos = p_of_keeping_infos / normalizing_factor
+        return p_of_keeping_infos
+
     def clear_all_memory(self):
-        for key in self.memory.keys:
+        for key in self.memory.keys():
             del self.memory[key]
-    
-    def clear_one_random_element(self):
-        key_to_delete = self.randomly_choose_which_key_to_clear()
+
+    def clear_one_element(self, current_position=0, random_position=False):
+        if random_position:
+            key_to_delete = random.choice(list(self.memory.keys()))
+        else:
+            key_to_delete = np.random.choice(list(self.memory.keys()), p=self.p_of_keeping_infos(current_position).tolist())
         del self.memory[key_to_delete]
 
     def add_element_in_position_n(self, n, word_embedding):
@@ -43,13 +50,13 @@ class Memory:
         if n in self.memory.keys():
             raise ValueError("The cell corresponding to word {n} is already occupied")
         if self.free_cells == 0:
-            self.clear_one_random_element()
-        self.memory[n]=word_embedding
+            self.clear_one_element()
+        self.memory[n] = word_embedding
 
-    def add_element(self, word_embedding):
+    def add_element(self, word_embedding, current_position, random_position=False):
         if self.free_cells == 0:
-            self.clear_one_random_element()
-        self.memory[self.occupied_cells]=word_embedding
+            self.clear_one_element(current_position=current_position, random_position=random_position, )
+        self.memory[current_position] = word_embedding # pay attention to current position, should always start from 0
 
     def __repr__(self):
         memory_infos = f""
@@ -59,6 +66,7 @@ class Memory:
 
     def __str__(self) -> str:
         return f"Memory with {self.number_of_cells} cells and {self.occupied_cells} occupied cells"
+
 
 if __name__ == "__main__":
     memory = Memory()

source/model.py
@@ -17,27 +17,44 @@ class Model:
     def move_one_word_forward(self):
         if self.current_position >= len(self.text.contextually_embedded_text) - 1:
             raise ValueError("The end of the text has been reached")
+        self.memory.add_element(self.text.contextually_embedded_text[self.current_position], self.current_position)
         self.current_position += 1
-        self.memory.add_element(self.text.contextually_embedded_text[self.current_position])
 
-    def calculate_information_presence_in_memory(self):
+    def calculate_information_presence_in_memory(self, one_hot=False):
         information_presence = []
+        i = -1
         # implementing the scalar product formula discussed in the notebook
         for word_embedding in self.text.embedded_text:
             info_about_word = 0
+            i += 1
             for vector in self.memory.memory.values():
-                info_about_word += np.dot(np.array(word_embedding), np.array(vector))
+                if one_hot:
+                    info_about_word += vector[i]
+                else:
+                    info_about_word += np.dot(np.array(word_embedding), np.array(vector))  # this scalar product can
+                # be reduced to a single component by using the fact that word embeddings are one hot encoded
             information_presence.append(info_about_word)
-        return information_presence
+        return information_presence[::-1]
+
+    def fit_power_law(self, information_presence):
+        """
+        Fits a power law to the information presence in memory
+        :param information_presence:
+        :return:
+        """
+        pass
 
     def plot_information_presence_in_memory(self, information_presence, show_labels=False):
         for i in range(len(information_presence)):
             if show_labels:
                 plt.scatter(i, information_presence[i], marker="o", label=model.text.text[i])
                 plt.legend(loc="upper right")
-            else:
-                plt.scatter(i, information_presence[i], marker="o")
-        plt.show()
+                plt.show()
+        if not show_labels:
+            plt.plot(information_presence)
+            plt.show()
+
+
 
     def __repr__(self):
         model_infos = f""